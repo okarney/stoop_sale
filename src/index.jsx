1162f518489ce63f8e9b7dd449cd656025dbfb2d
Trevor Nanney <trevornanney@gmail.com>
2024-07-12 04:47:44 -0500

    hopefully fixed countdown not being outside

--- src/index.jsx
+++ src/index.jsx
@@ -2,13 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css'; // Import your CSS file
 import Envelope from './Envelope'; // Import your Envelope component
-import CountdownTimer from './CountdownTimer'; // Import CountdownTimer component
 
 const Index = () => {
   return (
     <div>
       <Envelope />
-      <CountdownTimer targetDate="2024-08-01T00:00:00" /> {/* Add CountdownTimer here */}
     </div>
   );
 };
